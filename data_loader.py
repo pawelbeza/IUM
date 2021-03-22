9bc6a6caa29bb3ed7f4c6ec22420590501a651e0
Paweł Bęza <pawel.beza@grupawp.pl>
2021-03-22 22:09:37 +0100

    add purchase & time duration attribute

--- data_loader.py
+++ data_loader.py
@@ -2,6 +2,8 @@ from pathlib import Path
 import pandas as pd
 import jsonlines
 
+import datetime
+
 
 def jsonl_to_pd_dataframe(filepath):
     with jsonlines.open(filepath) as reader:
@@ -27,3 +29,41 @@ def get_merged_data():
     sessions_df = pd.merge(sessions_df, deliveries_df, how="left", on="purchase_id")
 
     return sessions_df
+
+
+def add_purchase_attrib(data):
+    for group_name, df_group in data.groupby('session_id'):
+        last_session_log = df_group.iloc[-1]
+
+        is_purchased = last_session_log['event_type'] == 'BUY_PRODUCT'
+
+        for row_index, row in df_group.iterrows():
+            data.loc[row_index, 'purchase'] = is_purchased
+
+
+def add_session_duration_attrib(data):
+    date_format = '%Y-%m-%dT%H:%M:%S'
+    for group_name, df_group in data.groupby('session_id'):
+        last_session_log = df_group.iloc[0]
+
+        session_date_start = datetime.datetime.strptime(last_session_log['timestamp'], date_format)
+        for row_index, row in df_group.iterrows():
+            session_date_end = datetime.datetime.strptime(row['timestamp'], date_format)
+            data.loc[row_index, 'duration'] = (session_date_end - session_date_start).total_seconds()
+
+
+def add_new_attributes(data):
+    add_session_duration_attrib(data)
+    add_purchase_attrib(data)
+
+
+def get_data():
+    sessions = get_merged_data()
+
+    add_new_attributes(sessions)
+
+    columns_to_drop = ['user_id', 'user_id', 'product_id', 'purchase_id', 'timestamp_event', 'purchase_timestamp',
+                       'street']
+    sessions = sessions.drop(columns_to_drop, axis=1)
+
+    return sessions

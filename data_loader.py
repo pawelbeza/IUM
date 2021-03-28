41190cb61f50d94a8862bd2a7c9ae139a7afc884
Paweł Bęza <pawel.beza@grupawp.pl>
2021-03-28 22:03:22 +0200

    Squashed commit of the following:
    commit e7a1f30159a3cdc6de2caf779ad0b54c1156f67c
    Author: Paweł Bęza <pawel.beza@grupawp.pl>
    Date:   Sun Mar 28 21:51:07 2021 +0200
    
        add missing values to user_id
    
    commit 998f5f2722bf02d798163fc7388ad19df5227cc4
    Author: Paweł Bęza <pawel.beza@grupawp.pl>
    Date:   Sun Mar 28 18:56:12 2021 +0200
    
        optimize operations on DataFrame
    
    commit f738d6df89ffacec37b4f1c910ff59d2003c568c
    Author: Paweł Bęza <pawel.beza@grupawp.pl>
    Date:   Sat Mar 27 16:09:36 2021 +0100
    
        remove city chart
    
    commit 69c091061bc0760f507df02665f4ed4747743e34
    Author: Paweł Bęza <pawel.beza@grupawp.pl>
    Date:   Sat Mar 27 16:08:56 2021 +0100
    
        add attributes related to events, time & product category

--- data_loader.py
+++ data_loader.py
@@ -5,13 +5,14 @@ import pandas
 import pandas as pd
 import jsonlines
 
-import datetime
+import datetime as dt
 
 
 def jsonl_to_pd_dataframe(filepath):
     with jsonlines.open(filepath) as reader:
         data = [obj for obj in reader]
-    return pd.DataFrame.from_records(data)
+
+    return pd.DataFrame(data)
 
 
 def get_merged_data():
@@ -35,57 +36,50 @@ def get_merged_data():
 
 
 def add_purchase_attrib(data, df_group):
-    last_session_log = df_group.iloc[-1]
+    last_event_log = df_group.iloc[-1].event_type
+    is_purchased = last_event_log == 'BUY_PRODUCT'
 
-    is_purchased = last_session_log['event_type'] == 'BUY_PRODUCT'
-
-    for row_index, row in df_group.iterrows():
-        data.loc[row_index, 'purchase'] = is_purchased
+    for index, row in df_group.iterrows():
+        data.at[index, 'purchase'] = is_purchased
 
 
 def add_time_specific_attribs(data, df_group):
-    date_format = '%Y-%m-%dT%H:%M:%S'
+    columns = df_group.columns
+    session_date_start = df_group.iat[0, columns.get_loc('timestamp')]
+    for index, row in df_group.iterrows():
+        session_date_end = row.timestamp
 
-    first_session_log = df_group.iloc[0]
-
-    session_date_start = datetime.datetime.strptime(first_session_log['timestamp'], date_format)
-    for row_index, row in df_group.iterrows():
-        session_date_end = datetime.datetime.strptime(row['timestamp'], date_format)
-
-        data.loc[row_index, 'duration'] = (session_date_end - session_date_start).total_seconds()
-        data.loc[row_index, 'weekend'] = session_date_end.weekday() >= 5
-        data.loc[row_index, 'month'] = session_date_end.strftime("%B")
-        data.loc[row_index, 'day'] = session_date_end.day
-        data.loc[row_index, 'hour'] = session_date_end.hour
+        data.at[index, 'duration'] = (session_date_end - session_date_start).total_seconds()
+        data.at[index, 'weekend'] = session_date_end.weekday() >= 5
+        data.at[index, 'month'] = session_date_end.strftime("%B")
+        data.at[index, 'day'] = session_date_end.day
+        data.at[index, 'hour'] = session_date_end.hour
 
 
 def add_event_specific_attribs(data, df_group):
-    date_format = '%Y-%m-%dT%H:%M:%S'
-
     total_viewed_items = 0
     product_set = set()
 
-    first_session_log = df_group.iloc[0]
-    session_date_start = datetime.datetime.strptime(first_session_log['timestamp'], date_format)
-
-    for row_index, row in df_group.iterrows():
-        session_date_end = datetime.datetime.strptime(row['timestamp'], date_format)
+    columns = df_group.columns
+    session_date_start = df_group.iat[0, columns.get_loc('timestamp')]
+    for index, row in df_group.iterrows():
+        session_date_end = row.timestamp
         minutes = (session_date_end - session_date_start).total_seconds() / 60.0
 
         total_viewed_items += 1
         product_set.add(row.product_id)
 
-        data.loc[row_index, 'unique_item_views'] = len(product_set)
-        data.loc[row_index, 'item_views'] = total_viewed_items
-        data.loc[row_index, 'click_rate'] = total_viewed_items / minutes if minutes != 0 else 0
+        data.at[index, 'unique_item_views'] = len(product_set)
+        data.at[index, 'item_views'] = total_viewed_items
+        data.at[index, 'click_rate'] = total_viewed_items / minutes if minutes != 0 else 0
 
 
 def add_product_specific_attrib(data, df_group):
     unique_categories = set()
-    for row_index, row in df_group.iterrows():
+    for index, row in df_group.iterrows():
         main_category = row.category_path.split(';', 1)[0] if not pandas.isna(row.category_path) else ''
         unique_categories.add(main_category)
-        data.loc[row_index, 'unique_categories'] = len(unique_categories)
+        data.at[index, 'unique_categories'] = len(unique_categories)
 
 
 def add_new_attributes(data):
@@ -96,12 +90,25 @@ def add_new_attributes(data):
         add_purchase_attrib(data, df_group)
 
 
+def fill_user_id_nan(data):
+    for _, df_group in data.groupby('session_id'):
+        occurrences = df_group['user_id'].value_counts().keys()
+        if len(occurrences) == 0:
+            continue
+
+        group_user_id = df_group['user_id'].value_counts().keys()[0]
+        for index, row in df_group.iterrows():
+            data.at[index, 'user_id'] = group_user_id
+
+
 def get_data():
     sessions = get_merged_data()
+    sessions['timestamp'] = sessions['timestamp'].apply(lambda x: dt.datetime.strptime(x, '%Y-%m-%dT%H:%M:%S'))
 
     add_new_attributes(sessions)
+    fill_user_id_nan(sessions)
 
-    columns_to_drop = ['product_id', 'purchase_id', 'timestamp', 'timestamp', 'event_type', 'product_name',
+    columns_to_drop = ['product_id', 'purchase_id', 'timestamp', 'event_type', 'product_name',
                        'category_path', 'price']
     sessions = sessions.drop(columns_to_drop, axis=1)
 
